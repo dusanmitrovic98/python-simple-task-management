1217bbb9333784447770887d8bc31139c3e3fc88
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-18 20:54:41 +0200

    Update simple-task-management.py

--- simple-task-management.py
+++ simple-task-management.py
@@ -48,3 +48,4 @@ def update_status(tasks, task_id):
         new_status = input("Enter new task status (True/False): ").lower()
         if new_status == 'true':
             task['status'] = True
+        elif new_status == 'false':

c6e1266333afecf4e5f7b7f964b5e477a2db0b2a
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-18 20:51:09 +0200

    Update simple-task-management.py

--- simple-task-management.py
+++ simple-task-management.py
@@ -36,3 +36,4 @@ def edit_task(tasks, task_id):
     if task:
         new_title = input("Enter new task title: ")
         new_description = input("Enter new task description (optional): ")
+        task['title'] = new_title

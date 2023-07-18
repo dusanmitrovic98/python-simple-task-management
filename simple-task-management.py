3332dc283e6a7b449d64aef8fff17c7783ade7f3
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-18 21:05:01 +0200

    Update simple-task-management.py

--- simple-task-management.py
+++ simple-task-management.py
@@ -79,3 +79,4 @@ def find_parent_task(tasks, task_id):
                 return task
             parent_task = find_parent_task(child_task['children'], task_id)
             if parent_task:
+                return parent_task
